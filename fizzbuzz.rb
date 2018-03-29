8f5bb42d79ab9171744f3dbe6bc8941bda0b6601
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-29 19:54:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,9 @@
 def fizzbuzz(n)
     if n % 3 == 0 && n % 5 == 0
-        puts "FizzBuzz"
+        "FizzBuzz"
     elsif n % 3 == 0
         puts "Fizz"
     elsif n % 5 == 0
         puts "Buzz"
+    end
 end
